edeb4c72636b3ef8c822be0eafd68b09bb0ac12b
woojin <verecahill@gmail.com>
2019-01-26 12:39:02 +0900

    added packages

---
+++ routes.js
@@ -0,0 +1,45 @@
+const fs = require('fs');
+
+const requestHandler = (req, res) => {
+    const url = req.url;
+    const method = req.method;
+
+    if (url === '/'){
+        res.write('<html>');
+        res.write('<head><title>Enter MEssage</title></head>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">send</button></form></body>');
+        res.write('</html>');
+        return res.end();
+    }
+    if (url === '/message' && method === 'POST'){
+        const body = [];
+        req.on('data', (chunk) => {
+            console.log(chunk);
+            body.push(chunk);
+    
+        });
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1];            
+            fs.writeFile('message.txt', message, (err) => {
+                res.statusCode = 302;
+                res.setHeader('Location', '/');
+                return res.end();
+            });
+        });
+    }
+    if(url === 'users'){
+        res.write('<html>');
+        res.write('<head><title>User Page</title></head>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">send</button></form></body>');
+        res.write('</html>');
+    }
+    res.setHeader('Content-Type', 'text/html');
+};
+
+// module.exports = {
+//     handler: requestHandler,
+//     someText: 'Some hard coded text'
+// };
+
+// module.exports.handler = requestHandler;
